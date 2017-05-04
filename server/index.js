5740e3db767ac3762fabd7cd07c406515549fe20
Ham Solo <trehorna@gmail.com>
2017-05-04 17:20:53 +0200

    reeeeeeeeeeeeeeeeee

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ app.get('*', (req, res) => {
 
 const http = require('http').createServer(app);
 
-http.listen(9000, () => {
-    console.log('Listening on port 9000!');
+http.listen(3000, () => {
+    console.log('Listening on port 3000!');
 });
 
 const io = require('socket.io')(http);
